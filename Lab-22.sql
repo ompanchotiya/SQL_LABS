275b007a877e267029cd4e6ac43bace81a2e55ec
ompanchotiya <ompanchotiya1203@gmail.com>
2026-08-21 11:25:49 +0530

    Add SQL queries for comprehensive customer order analysis in Lab-22

---
+++ Lab-22.sql
@@ -0,0 +1,274 @@
+CREATE TABLE CUSTOMER_ALL
+(
+ORDERID INT,
+CNAME VARCHAR(30),
+PRODUCT VARCHAR(30),
+CATEGORY VARCHAR(30),
+AMOUNT INT,
+ORDERYEAR INT,
+CITY VARCHAR(30)
+)
+
+INSERT INTO CUSTOMER_ALL
+VALUES
+(101,'RAHUL','LAPTOP','ELECTRONICS',65000,2024,'RAJKOT'),
+(102,'PRIYA','MOBILE','ELECTRONICS',25000,2023,'SURAT'),
+(103,'AMIT','TABLE','FURNITURE',12000,2022,'AHMEDABAD'),
+(104,'NEHA','CHAIR','FURNITURE',8000,2024,'BARODA'),
+(105,'VISHAL','TV','ELECTRONICS',45000,2025,'MORBI'),
+(106,'RIYA','SOFA','FURNITURE',30000,2023,'SURAT'),
+(107,'MEHUL','AC','ELECTRONICS',40000,2022,'RAJKOT'),
+(108,'KRUNAL','BED','FURNITURE',40000,2025,'JAMNAGAR')
+SELECT * FROM CUSTOMER_ALL;
+
+
+--Part – A:
+--1. Display top 3 highest amount orders.
+WITH HIGHEST_AMT AS(
+     SELECT TOP 3 CNAME, AMOUNT FROM CUSTOMER_ALL
+     ORDER BY AMOUNT DESC
+)
+SELECT * FROM HIGHEST_AMT;
+--2. Display second highest order amount.
+WITH SECOND_HIGH AS(
+     SELECT *,
+     DENSE_RANK() OVER(ORDER BY AMOUNT DESC) AS D_R
+     FROM CUSTOMER_ALL
+)
+SELECT * FROM SECOND_HIGH
+WHERE D_R = 2
+--3. Display customers whose order amount is greater than category average amount.
+WITH GRATER_THAN_AVG AS(
+     SELECT CATEGORY, AVG(AMOUNT) AS AVG_AMT FROM CUSTOMER_ALL
+     GROUP BY CATEGORY
+)
+SELECT *
+FROM CUSTOMER_ALL C JOIN GRATER_THAN_AVG G
+ON C.CATEGORY = G.CATEGORY
+WHERE AMOUNT > AVG_AMT
+--4. Display categories having average amount greater than 30000.
+WITH AVG_GRATER_THAN_30000 AS(
+     SELECT CATEGORY, AVG(AMOUNT) AS AVG_AMT FROM CUSTOMER_ALL
+     GROUP BY CATEGORY
+)
+SELECT * FROM AVG_GRATER_THAN_30000
+WHERE AVG_AMT > 30000
+--5. Display highest amount order from each category.
+WITH HIGH_AMOUNT AS(
+     SELECT *,
+     DENSE_RANK() OVER(PARTITION BY CATEGORY ORDER BY AMOUNT DESC) AS D_R
+     FROM CUSTOMER_ALL
+)
+SELECT * FROM HIGH_AMOUNT
+WHERE D_R = 1
+--6. Display lowest amount order from each category.
+WITH HIGH_AMOUNT AS(
+     SELECT *,
+     DENSE_RANK() OVER(PARTITION BY CATEGORY ORDER BY AMOUNT ASC) AS D_R
+     FROM CUSTOMER_ALL
+)
+SELECT * FROM HIGH_AMOUNT
+WHERE D_R = 1
+--7. Display categories having more than 3 orders.
+WITH MORE_THAN_3_ORDER AS(
+     SELECT CATEGORY, COUNT(ORDERID) AS TOTAL_ORDER FROM CUSTOMER_ALL
+     GROUP BY CATEGORY
+)
+SELECT * FROM MORE_THAN_3_ORDER
+WHERE TOTAL_ORDER > 3
+--8. Display city-wise total order amount.
+WITH TOTAL_ORDER AS(
+     SELECT CITY, COUNT(ORDERID) AS TOTAL_ORDER FROM CUSTOMER_ALL
+     GROUP BY CITY
+)
+SELECT * FROM TOTAL_ORDER;
+--9. Display category having highest average order amount.
+WITH AVG_AMOUNT AS(
+     SELECT CATEGORY, AVG(AMOUNT) AS AVG_AMT FROM CUSTOMER_ALL
+     GROUP BY CATEGORY
+),
+CTE AS(
+     SELECT MAX(AVG_AMT) AS MAX_AMOUNT FROM AVG_AMOUNT
+)
+SELECT *
+FROM AVG_AMOUNT A JOIN CTE C
+ON A.AVG_AMT = C.MAX_AMOUNT
+--10. Display cumulative order amount in ascending order of amount.
+WITH C_ORDER AS(
+     SELECT *, SUM(AMOUNT) OVER(ORDER BY AMOUNT ASC) AS SUM_ALL
+     FROM CUSTOMER_ALL
+)
+SELECT * FROM C_ORDER;
+
+
+--Part – B:
+--11. Display category-wise top 2 highest amount orders.
+WITH TOP_2_AMOUNT AS(
+     SELECT *,
+     DENSE_RANK() OVER(PARTITION BY CATEGORY ORDER BY AMOUNT DESC) AS D_R
+     FROM CUSTOMER_ALL
+)
+SELECT * FROM TOP_2_AMOUNT
+WHERE D_R <= 2
+--12. Display customers whose amount is closest to category average amount.
+WITH AVG_AMOUNT AS(
+     SELECT CATEGORY, AMOUNT, AVG(AMOUNT) OVER(PARTITION BY CATEGORY) AS AVG_AMT FROM CUSTOMER_ALL
+),
+DIFF_AVG AS(
+     SELECT *, ABS(AMOUNT-AVG_AMT) AS DIFF FROM AVG_AMOUNT
+),
+RANK_DIFF AS(
+     SELECT *,
+     DENSE_RANK() OVER(ORDER BY DIFF ASC) AS D_R FROM DIFF_AVG
+)
+SELECT * FROM RANK_DIFF
+WHERE D_R = 1
+--13. Display previous, current and next order amount together.
+WITH ORDER_DATA AS
+(
+    SELECT ORDERID,
+           CNAME,
+           AMOUNT AS CURRENT_AMOUNT,
+
+           LAG(AMOUNT) OVER
+           (
+               ORDER BY ORDERID
+           ) AS PREVIOUS_AMOUNT,
+
+           LEAD(AMOUNT) OVER
+           (
+               ORDER BY ORDERID
+           ) AS NEXT_AMOUNT
+
+    FROM CUSTOMER_ALL
+)
+SELECT *
+FROM ORDER_DATA;
+--14. Display customers whose amount is greater than previous customer's amount.
+WITH PREVIOUS_AMOUNT AS
+(
+    SELECT *,
+           LAG(AMOUNT) OVER
+           (
+               ORDER BY ORDERID
+           ) AS PREV_AMOUNT
+    FROM CUSTOMER_ALL
+)
+SELECT *
+FROM PREVIOUS_AMOUNT
+WHERE AMOUNT > PREV_AMOUNT;
+--15. Display customers whose rank and dense rank are different.
+WITH RANK_DATA AS
+(
+    SELECT *,
+           RANK() OVER
+           (
+               ORDER BY AMOUNT DESC
+           ) AS RNK,
+
+           DENSE_RANK() OVER
+           (
+               ORDER BY AMOUNT DESC
+           ) AS DENSE_RNK
+    FROM CUSTOMER_ALL
+)
+SELECT *
+FROM RANK_DATA
+WHERE RNK <> DENSE_RNK;
+
+--Part – C:
+--16. Display orders whose amount is neither highest nor lowest in their category.
+
+WITH CATEGORY_RANK AS
+(
+    SELECT *,
+           RANK() OVER
+           (
+               PARTITION BY CATEGORY
+               ORDER BY AMOUNT DESC
+           ) AS HIGH_RANK,
+
+           RANK() OVER
+           (
+               PARTITION BY CATEGORY
+               ORDER BY AMOUNT ASC
+           ) AS LOW_RANK
+
+    FROM CUSTOMER_ALL
+)
+SELECT *
+FROM CATEGORY_RANK
+WHERE HIGH_RANK <> 1
+AND LOW_RANK <> 1;
+
+--17. Display category-wise difference between highest and lowest amount.
+WITH CATEGORY_AMOUNT AS
+(
+    SELECT CATEGORY,
+           MAX(AMOUNT) AS HIGHEST_AMOUNT,
+           MIN(AMOUNT) AS LOWEST_AMOUNT
+    FROM CUSTOMER_ALL
+    GROUP BY CATEGORY
+)
+SELECT CATEGORY,
+       HIGHEST_AMOUNT,
+       LOWEST_AMOUNT,
+       HIGHEST_AMOUNT - LOWEST_AMOUNT AS DIFFERENCE
+FROM CATEGORY_AMOUNT;
+
+--18. Display customers whose amount is greater than all FURNITURE category orders.
+WITH FURNITURE_MAX AS
+(
+    SELECT MAX(AMOUNT) AS MAX_AMOUNT
+    FROM CUSTOMER_ALL
+    WHERE CATEGORY = 'FURNITURE'
+)
+SELECT *
+FROM CUSTOMER_ALL
+WHERE AMOUNT > 
+(
+    SELECT MAX_AMOUNT
+    FROM FURNITURE_MAX
+);
+--19. Display categories where all orders are above 10000.
+
+WITH CATEGORY_MIN AS
+(
+    SELECT CATEGORY,
+           MIN(AMOUNT) AS MIN_AMOUNT
+    FROM CUSTOMER_ALL
+    GROUP BY CATEGORY
+)
+SELECT *
+FROM CATEGORY_MIN
+WHERE MIN_AMOUNT > 10000;
+--20. Display customers whose amount difference from category topper is minimum.
+WITH CATEGORY_MAX AS
+(
+    SELECT CATEGORY,
+           MAX(AMOUNT) AS TOP_AMOUNT
+    FROM CUSTOMER_ALL
+    GROUP BY CATEGORY
+),
+DIFFERENCE_DATA AS
+(
+    SELECT C.*,
+           CM.TOP_AMOUNT,
+           CM.TOP_AMOUNT - C.AMOUNT AS DIFFERENCE
+    FROM CUSTOMER_ALL C
+    JOIN CATEGORY_MAX CM
+    ON C.CATEGORY = CM.CATEGORY
+),
+RANK_DATA AS
+(
+    SELECT *,
+           RANK() OVER
+           (
+               PARTITION BY CATEGORY
+               ORDER BY DIFFERENCE ASC
+           ) AS RNK
+    FROM DIFFERENCE_DATA
+)
+SELECT *
+FROM RANK_DATA
+WHERE RNK = 1;
